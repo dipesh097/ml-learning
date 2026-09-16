bd9eb47098bba84ff4e2214c1e15a2d455cd1558
dipesh097 <deepanshu966060@gmail.com>
2026-09-17 00:01:31 +0530

    refactor: Remove old filename open cv/open cv basics/phase 02/06-drawing line.py

--- open cv/open cv basics/phase 02/06-drawing line.py
+++
@@ -1,9 +0,0 @@
-
-import cv2
-
-image=cv2.imread(r"C:\Users\dipesh's-device\Pictures\Camera Roll\Screenshots\Screenshot 2025-10-27 210150.png")
-
-cv2.line(image,(100,500),(1000,800),(0,255,255),20)
-cv2.imshow("image",image)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
