ca772a270f95df5600d05448d0fc043ef75fa599
dipesh097 <deepanshu966060@gmail.com>
2026-09-17 00:00:27 +0530

    refactor: Remove old filename open cv/open cv basics/phase 01/01-loaded image showing.py

--- open cv/open cv basics/phase 01/01-loaded image showing.py
+++
@@ -1,9 +0,0 @@
-import cv2
-
-image=cv2.imread(r"C:\Users\dipesh's-device\Pictures\Camera Roll\WIN_20260409_01_27_28_Pro.jpg")
-
-gray_image=cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
-
-cv2.imshow("hello",gray_image)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
